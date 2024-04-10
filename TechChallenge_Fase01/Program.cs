2871215cb372ad54df4f37d1fa8d78d842c4b5d6
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2024-04-10 17:26:02 -0300

    Adding db connection string

--- TechChallenge_Fase01/Program.cs
+++ TechChallenge_Fase01/Program.cs
@@ -7,13 +7,22 @@ using TechChallenge_Fase01.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddValidatorsFromAssemblyContaining<ContactRequestValidator>();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
-    opt.UseInMemoryDatabase("Contacts");
+    opt.UseSqlServer(configuration.GetConnectionString("Contacts"),
+        sqlServerOptions =>
+        {
+            sqlServerOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+            sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 4, maxRetryDelay: TimeSpan.FromSeconds(3), errorNumbersToAdd: []);
+        });
 });
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
