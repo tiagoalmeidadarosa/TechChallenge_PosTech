dc6e7ce7d95fca04f8be95137a1a3b102b4742a5
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2024-05-01 14:31:40 -0300

    Fixes after merge

---
+++ TechChallenge_Fase01.Tests/Fakers/ContactFaker.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using TechChallenge_Fase01.Core.Entities;
+
+namespace TechChallenge_Fase01.Tests.Fakers
+{
+    public class ContactFaker : Faker<Contact> 
+    {
+        public ContactFaker(string locale) : base(locale)
+        {
+            RuleFor(x => x.Id, f => f.UniqueIndex);
+            RuleFor(x => x.Name, f => f.Person.FullName);
+            RuleFor(x => x.Phone, f => f.Random.ReplaceNumbers($"99#######"));
+            RuleFor(x => x.Email, f => f.Person.Email);
+            RuleFor(x => x.DDD, f => f.Random.Number(11, 99));
+        }
+    }
+}
